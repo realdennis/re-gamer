05ceb96bd6eaae00cd39e22bbe247884b564fde0
realdennis <evolon688@gmail.com>
2018-12-26 08:18:39 +0800

    Finish

--- src/View/InBoard/ArticleItem.js
+++ src/View/InBoard/ArticleItem.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-const ArticleItem = ({ article }) => (
-  <li className="article-title" >
-    <Link
-      to={{
-        pathname: `/article/${article.bsn}/${article.snA}`,
-        name: article.title
-      }}
-    >
-      <p>{article.title}</p>
-    </Link>
-  </li>
-);
-export default ArticleItem;
+import styled from 'styled-components';
+const numToColor = num => {
+  if(num>999) return 'fuchsia';
+  if(num>99) return 'red';
+  if(num>9 ) return 'yellow';
+  return 'lime'
+}
+const ArticleItem = ({ className, article }) => {
+  return (
+    <li className={className}>
+      <Link
+        to={{
+          pathname: `/article/${article.bsn}/${article.snA}`,
+          name: article.title
+        }}
+      >
+        <p className="gp" style={{color:numToColor(article.gp)}}>{article.gp}</p>
+        <p className="title">{article.title}</p>
+      </Link>
+    </li>
+  );
+};
+export default styled(ArticleItem)`
+  p.gp {
+    width: 10%;
+    min-width: 40px;
+    flex-shrink: 0;
+    text-align: center;
+  }
+`;
