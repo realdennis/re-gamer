d96c8b861a12b7112882837ed7fedae1beba2f3f
realdennis <evolon688@gmail.com>
2018-12-30 02:39:30 +0800

    Updates

---
+++ precache-manifest.d68ff3a3528041732a187a9b92b3ce27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b049bcbf1beb6f159268",
+    "url": "/re-gamer/static/css/main.0de91059.chunk.css"
+  },
+  {
+    "revision": "b049bcbf1beb6f159268",
+    "url": "/re-gamer/static/js/main.b049bcbf.chunk.js"
+  },
+  {
+    "revision": "01000f0c4ba03015ac5a",
+    "url": "/re-gamer/static/css/1.86d813db.chunk.css"
+  },
+  {
+    "revision": "01000f0c4ba03015ac5a",
+    "url": "/re-gamer/static/js/1.01000f0c.chunk.js"
+  },
+  {
+    "revision": "4632c9c998ded1514f73",
+    "url": "/re-gamer/static/js/runtime~main.4632c9c9.js"
+  },
+  {
+    "revision": "cd1c2e92e0e46b9cd61de70939ebf831",
+    "url": "/re-gamer/index.html"
+  }
+];
